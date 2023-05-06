c72758c70c1403d6f80d631ca2ab3973d9060b07
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-05-06 10:41:36 +0300

    core-transactions:handlers:two:ipfs

---
+++ packages/core-transactions/src/handlers/two/ipfs.ts
@@ -0,0 +1,144 @@
+import { Container, Contracts, Utils as AppUtils } from "@smartholdem/core-kernel";
+import { Interfaces, Managers, Transactions, Utils } from "@smartholdem/crypto";
+
+import { MempoolIndexes } from "../../enums";
+import { IpfsHashAlreadyExists } from "../../errors";
+import { TransactionHandler, TransactionHandlerConstructor } from "../transaction";
+
+// todo: revisit the implementation, container usage and arguments after core-database rework
+// todo: replace unnecessary function arguments with dependency injection to avoid passing around references
+@Container.injectable()
+export class IpfsTransactionHandler extends TransactionHandler {
+    @Container.inject(Container.Identifiers.TransactionPoolMempoolIndexRegistry)
+    private readonly mempoolIndexRegistry!: Contracts.TransactionPool.MempoolIndexRegistry;
+
+    @Container.inject(Container.Identifiers.TransactionHistoryService)
+    @Container.tagged("connection", "default")
+    private readonly transactionHistoryService!: Contracts.Shared.TransactionHistoryService;
+
+    public dependencies(): ReadonlyArray<TransactionHandlerConstructor> {
+        return [];
+    }
+
+    public walletAttributes(): ReadonlyArray<string> {
+        return ["ipfs", "ipfs.hashes"];
+    }
+
+    public getConstructor(): Transactions.TransactionConstructor {
+        return Transactions.Two.IpfsTransaction;
+    }
+
+    public async bootstrap(): Promise<void> {
+        const criteria = {
+            typeGroup: this.getConstructor().typeGroup,
+            type: this.getConstructor().type,
+        };
+
+        for await (const transaction of this.transactionHistoryService.streamByCriteria(criteria)) {
+            AppUtils.assert.defined<string>(transaction.senderPublicKey);
+            AppUtils.assert.defined<string>(transaction.asset?.ipfs);
+
+            const wallet = this.walletRepository.findByPublicKey(transaction.senderPublicKey);
+            if (!wallet.hasAttribute("ipfs")) {
+                wallet.setAttribute("ipfs", { hashes: {} });
+            }
+
+            const ipfsHashes: Contracts.State.WalletIpfsAttributes = wallet.getAttribute("ipfs.hashes");
+            ipfsHashes[transaction.asset.ipfs] = true;
+            this.walletRepository.index(wallet);
+        }
+    }
+
+    public async isActivated(): Promise<boolean> {
+        return Managers.configManager.getMilestone().aip11 === true;
+    }
+
+    public async throwIfCannotEnterPool(transaction: Interfaces.ITransaction): Promise<void> {
+        AppUtils.assert.defined<string>(transaction.data.asset?.ipfs);
+
+        if (this.mempoolIndexRegistry.get(MempoolIndexes.Ipfs).has(transaction.data.asset.ipfs)) {
+            throw new Contracts.TransactionPool.PoolError(
+                `IPFS transaction with IPFS address "${transaction.data.asset.ipfs}" already in the pool`,
+                "ERR_PENDING",
+            );
+        }
+    }
+
+    public async getInvalidPoolTransactions(transaction: Interfaces.ITransaction): Promise<Interfaces.ITransaction[]> {
+        AppUtils.assert.defined<string>(transaction.data.asset?.ipfs);
+
+        const ipfsIndex = this.mempoolIndexRegistry.get(MempoolIndexes.Ipfs);
+
+        return ipfsIndex.has(transaction.data.asset.ipfs) ? [ipfsIndex.get(transaction.data.asset.ipfs)] : [];
+    }
+
+    public async onPoolEnter(transaction: Interfaces.ITransaction): Promise<void> {
+        AppUtils.assert.defined<string>(transaction.data.asset?.ipfs);
+
+        this.mempoolIndexRegistry.get(MempoolIndexes.Ipfs).set(transaction.data.asset.ipfs, transaction);
+    }
+
+    public async onPoolLeave(transaction: Interfaces.ITransaction): Promise<void> {
+        AppUtils.assert.defined<string>(transaction.data.asset?.ipfs);
+
+        this.mempoolIndexRegistry.get(MempoolIndexes.Ipfs).forget(transaction.data.asset.ipfs);
+    }
+
+    public async throwIfCannotBeApplied(
+        transaction: Interfaces.ITransaction,
+        wallet: Contracts.State.Wallet,
+    ): Promise<void> {
+        if (Utils.isException(transaction.data)) {
+            return;
+        }
+
+        AppUtils.assert.defined<Interfaces.IHtlcLockAsset>(transaction.data.asset?.ipfs);
+
+        if (this.walletRepository.hasByIndex(Contracts.State.WalletIndexes.Ipfs, transaction.data.asset.ipfs)) {
+            throw new IpfsHashAlreadyExists();
+        }
+
+        return super.throwIfCannotBeApplied(transaction, wallet);
+    }
+
+    public async applyToSender(transaction: Interfaces.ITransaction): Promise<void> {
+        await super.applyToSender(transaction);
+
+        AppUtils.assert.defined<string>(transaction.data.senderPublicKey);
+
+        const sender: Contracts.State.Wallet = this.walletRepository.findByPublicKey(transaction.data.senderPublicKey);
+
+        if (!sender.hasAttribute("ipfs")) {
+            sender.setAttribute("ipfs", { hashes: {} });
+        }
+
+        AppUtils.assert.defined<string>(transaction.data.asset?.ipfs);
+
+        sender.getAttribute("ipfs.hashes", {})[transaction.data.asset.ipfs] = true;
+
+        this.walletRepository.index(sender);
+    }
+
+    public async revertForSender(transaction: Interfaces.ITransaction): Promise<void> {
+        await super.revertForSender(transaction);
+
+        AppUtils.assert.defined<string>(transaction.data.senderPublicKey);
+
+        const sender: Contracts.State.Wallet = this.walletRepository.findByPublicKey(transaction.data.senderPublicKey);
+
+        AppUtils.assert.defined<Interfaces.ITransactionAsset>(transaction.data.asset?.ipfs);
+
+        const ipfsHashes = sender.getAttribute("ipfs.hashes");
+        delete ipfsHashes[transaction.data.asset.ipfs];
+
+        if (!Object.keys(ipfsHashes).length) {
+            sender.forgetAttribute("ipfs");
+        }
+
+        this.walletRepository.index(sender);
+    }
+
+    public async applyToRecipient(transaction: Interfaces.ITransaction): Promise<void> {}
+
+    public async revertForRecipient(transaction: Interfaces.ITransaction): Promise<void> {}
+}
