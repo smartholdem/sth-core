0d763fbdb7fd2248c2364c949d47364f84e8c932
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-05-10 10:17:29 +0300

    core-api:controllers:blockchain

---
+++ packages/core-api/src/controllers/blockchain.ts
@@ -0,0 +1,31 @@
+import { Container, Contracts, Utils } from "@smartholdem/core-kernel";
+
+import { Controller } from "./controller";
+
+export class BlockchainController extends Controller {
+    @Container.inject(Container.Identifiers.WalletRepository)
+    @Container.tagged("state", "blockchain")
+    private walletRepository!: Contracts.State.WalletRepository;
+
+    @Container.inject(Container.Identifiers.StateStore)
+    private readonly stateStore!: Contracts.State.StateStore;
+
+    public async index() {
+        const { data } = this.stateStore.getLastBlock();
+
+        const burnWallet = this.walletRepository.getBurnWallet();
+        const supply = Utils.supplyCalculator.calculate(data.height);
+
+        return {
+            data: {
+                block: {
+                    height: data.height,
+                    id: data.id,
+                },
+                supply: supply.minus(burnWallet.getBalance()).toFixed(),
+                generated: supply.toFixed(),
+                burned: burnWallet.getBalance().toFixed(),
+            },
+        };
+    }
+}
