bf34628dba2d3127ea4ebf36d68b0f632aa27a43
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-08-29 16:11:03 +0300

    core-p2p

---
+++ packages/core-p2p/src/actions/validate-and-accept-peer.ts
@@ -0,0 +1,21 @@
+import { Container, Contracts, Services, Types } from "@smartholdem/core-kernel";
+import { PeerProcessor } from "../peer-processor";
+
+export class ValidateAndAcceptPeerAction extends Services.Triggers.Action {
+    private app: Contracts.Kernel.Application;
+
+    public constructor(app: Contracts.Kernel.Application) {
+        super();
+        this.app = app;
+    }
+
+    public async execute(args: Types.ActionArguments): Promise<void> {
+        const peer: Contracts.P2P.Peer = args.peer;
+        const headers: Contracts.P2P.Headers = args.headers;
+        const options: Contracts.P2P.AcceptNewPeerOptions = args.options;
+
+        return this.app
+            .get<PeerProcessor>(Container.Identifiers.PeerProcessor)
+            .validateAndAcceptPeer(peer, headers, options);
+    }
+}
